ac354fe57034f94a055e511b64c9430e316c5a90
syafiqfadillah <codingan77@gmail.com>
2022-10-13 22:10:05 +0700

    pr

---
+++ 13-10-2022/pr/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+const express = require("express")
+const app = express()
+
+app.use("/", (req, res, next) => {
+	res.send(fs.readFileSync("./ui.html", "utf-8"))
+	next()
+})
+
+app.listen(4000, () => {
+	console.log("Server Running...")
+})
