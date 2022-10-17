59ee575e64924da3274aab164db96f64fab3bf59
syafiqfadillah <codingan77@gmail.com>
2022-10-17 11:53:57 +0700

    perubahan

--- project1/script.js
+++ project1/script.js
@@ -1,29 +1,28 @@
-function changeColor(i, r, g) {
-	if (getComputedStyle(r[i]).backgroundColor != "rgb(0, 0, 0)") {
+function changeColor(i, obj) {
 	// search a button with black background-color
-	r.forEach(q => {
-		if (getComputedStyle(q).backgroundColor == "rgb(0, 0, 0)") {
+	Object.keys(obj).forEach(q => {
+		if (getComputedStyle(obj[q][0]).backgroundColor == "rgb(34, 40, 49)") {
 			// if forEach find it, change the color
-			q.setAttribute("style", "background-color:white")
+			obj[q][0].setAttribute("style", "background-color:white")
 		}
 	})
-		// set selected button to black background-color
-		g[i].setAttribute("style", "background-color:black")
-	}
+	// set selected button to black background-color
+	obj[i][0].setAttribute("style", "background-color:#222831")
 }
 
 var r = []
+let u = ["0", "-20%", "-40%"]
 var g = document.getElementsByClassName("bar")
 
 for (let i=0; i<g.length; i++) {
-	r.push(g[i])
+	r[i] = [g[i], u[i]]
 }
 
 var slide = document.getElementsByClassName("s1")[0]
 
-for (let i=0; i<r.length; i++) {
-	r[i].addEventListener("click", () => {
-		changeColor(i, r, g)
+for (let i=0; i<Object.keys(r).length; i++) {
+	r[i][0].addEventListener("click", () => {
+		changeColor(i, r)
 		
 		switch (i) {
 			case 0:
@@ -39,6 +38,16 @@ for (let i=0; i<r.length; i++) {
 	})
 }
 
+// automatic
+let i = 0
+setInterval(()=>{
+	changeColor(i, r)
+	slide.setAttribute("style", `margin-left:${u[i]}`)
+	i++
+	if (i == u.length) {
+		i = 0
+	}
+}, 3000)
 
 
 
