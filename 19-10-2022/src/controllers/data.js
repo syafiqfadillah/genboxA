1235de72783d3965227f50e863b253d9a471b7b3
syafiqfadillah <codingan77@gmail.com>
2022-10-19 17:24:41 +0700

    perubahan

--- 19-10-2022/src/controllers/data.js
+++ 19-10-2022/src/controllers/data.js
@@ -1,5 +1,7 @@
 const BlogPost = require("../models/data")
 const {validationResult} = require("express-validator")
+const path = require("path")
+const fs = require("fs")
 
 const post = (req, res) => {
     const errors = validationResult(req)
@@ -36,7 +38,7 @@ const post = (req, res) => {
         .catch(err => console.error(err))
 }
 
-const data = (req, res) => {
+const datas = (req, res) => {
     BlogPost.find()
         .then(respon => {
             res.status(200).json({
@@ -46,4 +48,94 @@ const data = (req, res) => {
         })
 }
 
-module.exports = {data, post}
+const blog = (req, res) => {
+    const blogId = req.params.getId
+    BlogPost.findById(blogId)
+        .then(respon => {
+            res.status(200).json({
+                message: "blog berhasil dipanggil",
+                data: respon
+            })
+        })
+}
+
+const edit = (req, res, next) => {
+    const errors = validationResult(req)
+    
+    if (!errors.isEmpty()) {
+        const err = new Error("Invalid!")
+        err.errorStatus = 400
+        err.data = errors.array()
+        throw err
+    }
+
+    if (!req.file) {
+        const err = new Error("Gambar Harus Di Upload")
+        err.status = 422
+        throw err
+    }
+
+    let result = {
+        gambar: req.file.path,
+        penulis: req.body.penulis,
+        judul: req.body.judul,
+        isi: req.body.isi,
+    }
+
+    const blogId = req.params.getId
+
+    BlogPost.findById(blogId)
+        .then(respon => {
+            if (!respon) {
+                const err = new Error("data tidak ditemukan")
+                err.status(422)
+                throw err
+            }
+
+            respon.judul = result.judul
+            respon.gambar = result.gambar
+            respon.isi = result.isi
+            respon.penulis = result.penulis
+
+            return respon.save()
+        })
+        .then(respon => {
+            res.status(200).json({
+                message: "data berhasil di update",
+                data: respon
+            })
+        })
+        .catch(err => {
+            next(err)
+        })
+
+}
+
+const del = (req, res, next) => {
+    const blogId = req.params.getId
+    BlogPost.findById(blogId)
+        .then(respon => {
+            if (!respon) {
+                const err = new Error("blog tidak ditemukan")
+                err.status(422)
+                throw err
+            }
+
+            hapusGambar(respon.gambar)
+            return BlogPost.findByIdAndDelete(blogId)
+        })
+        .then(respon => {
+            res.status(200).json({
+                message: "data berhasil di hapus",
+                data: respon
+            })
+        })
+        .catch(err => next(err))
+}
+
+const hapusGambar = (file) => {
+    file = path.join(__dirname + "../.." + file)
+    fs.unlink(file, err => console.log(err))
+}
+
+module.exports = {blog, datas, post, edit, del}
--- 19-10-2022/src/routes/data.js
+++ 19-10-2022/src/routes/data.js
@@ -2,13 +2,24 @@ const express = require("express")
 const {body} = require("express-validator")
 const router = express.Router()
 
-const {data, post} = require("../controllers/data")
+const {blog, datas, post, edit, del} = require("../controllers/data")
 
-router.get("/data", data)
+router.get("/data", datas)
+
 router.post("/post", [
     body("judul").isLength({min: 5}).withMessage("Panjang Judul Kurang!"),
     body("isi").isLength({min: 10}).withMessage("Panjang Isi Kurang!"),
     body("penulis").isLength({min: 3}).withMessage("Panjang Penulis Kurang!")
 ], post)
+
+router.get("/data/:getId", blog)
+
+router.put("/data/:getId", [
+    body("judul").isLength({min: 5}).withMessage("Panjang Judul Kurang!"),
+    body("isi").isLength({min: 10}).withMessage("Panjang Isi Kurang!"),
+    body("penulis").isLength({min: 3}).withMessage("Panjang Penulis Kurang!")
+], edit)
+
+router.delete("/data/:getId", del)
 
 module.exports = router
